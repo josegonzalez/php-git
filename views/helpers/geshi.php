8b145dcc7a172ae60b70aa71ebc7fc2f44a939df
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-10-17 19:27:38 -0400

    fixed setting of source and language on highlighted geshi code

--- views/helpers/geshi.php
+++ views/helpers/geshi.php
@@ -7,8 +7,11 @@ class GeshiHelper {
     function highlight($source, $language = 'diff') {
         if (!$this->_geshi) {
             require_once(LIBS . 'geshi.php');
-            $this->_geshi = new GeSHi($source, $language);
+            $this->_geshi = new GeSHi();
         }
+
+        $this->_geshi->set_source($source);
+        $this->_geshi->set_language($language);
 
         return $this->_geshi->parse_code();
     }
