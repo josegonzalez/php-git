402b5db992d9ae1633a636867453a77dc78ee129
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-10-20 12:17:01 -0400

    commits/index shows an index of commits

--- controllers/commits_controller.php
+++ controllers/commits_controller.php
@@ -2,6 +2,24 @@
 
 class CommitsController extends Controller {
 
+    function index() {
+        Git::loadRepositories($this->_config);
+
+        $this->_breadcrumbs = array(
+            'home' => '/',
+            $this->_request->params['project'] => "/{$this->_request->params['project']}",
+        );
+
+        $this->_config['title'] = $this->_request->params['project'];
+        $owner      = $this->Project->getOwner($this->_request->params['project']);
+        $last_change= $this->Project->getLastChange($this->_request->params['project']);
+        $description= $this->Project->getDescription($this->_request->params['project']);
+        $tags       = $this->Project->getTags($this->_request->params['project']);
+        $branches   = $this->Project->getBranches($this->_request->params['project']);
+        $shortlogs  = $this->Project->getShortlog($this->_request->params['project']);
+        $this->set(compact('owner', 'last_change', 'description', 'tags', 'branches', 'shortlogs'));
+    }
+
     function show() {
         Git::loadRepositories($this->_config);
         $this->_config['title'] = 'Commit' . $this->_request->params['commit'];
