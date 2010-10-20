4b83368e95e0e9433f478336dccba6af11bf7eb1
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-10-20 12:16:21 -0400

    updated project/show to show a tree of the repo instead of a list of commits

--- controllers/projects_controller.php
+++ controllers/projects_controller.php
@@ -11,12 +11,10 @@ class ProjectsController extends Controller {
         );
 
         $this->_config['title'] = $this->_request->params['project'];
-        $owner      = $this->Project->getOwner($this->_request->params['project']);
-        $last_change= $this->Project->getLastChange($this->_request->params['project']);
-        $description= $this->Project->getDescription($this->_request->params['project']);
-        $tags       = $this->Project->getTags($this->_request->params['project']);
-        $branches   = $this->Project->getBranches($this->_request->params['project']);
-        $shortlogs  = $this->Project->getShortlog($this->_request->params['project']);
-        $this->set(compact('owner', 'last_change', 'description', 'tags', 'branches', 'shortlogs'));
+        $tree = $this->Project->getTree(
+            $this->_request->params['project'],
+            $this->_request->params['filepath']
+        );
+        $this->set(compact('tree'));
     }
 }
--- views/projects/show.php
+++ views/projects/show.php
@@ -1,23 +1,21 @@
-<div class="gitspacer">&nbsp;</div>
-    <?php echo $this->element('summary_title'); ?>
-<div class="gitspacer">&nbsp;</div>
-<?php echo $this->element('description', compact('description', 'owner', 'last_change')); ?>
 <div class="table-wrapper">
-    <table class="commitlog" cellpadding="0" cellspacing="0">
-        <?php $i = 0; foreach ($shortlogs as $log) : ?>
-            <tr<?php if ($i++%2==0) echo ' class="altrow"'?>>
-                <td><?php echo $this->Gravatar->image($log['email']); ?></td>
+    <table class="listing" cellpadding="0" cellspacing="0">
+        <tr>
+            <th>file</th>
+            <th>perm</th>
+        </tr>
+        <?php foreach ($tree as $blob) : ?>
+            <tr>
                 <td>
-                    <?php echo $log['subject']; ?><br />
-                    <?php echo $log['author']; ?> (author)<br />
-                    <?php echo date('M j, Y', $log['timestamp']); ?>
+                    <?php if ($blob['type'] == 'tree') : ?>
+                        <img src="/img/16px/folder.png" />
+                    <?php else : ?>
+                        <?php echo $this->File->image($blob['file']); ?>
+                    <?php endif; ?>
+                    <?php echo $this->File->link($blob, $request); ?>
                 </td>
-                <td class="actions">
-                    <?php echo $this->Commit->link($log['hash'], $this->_request); ?><br />
-                    <?php echo $this->Commit->tree($log['tree'], $this->_request); ?><br />
-                    <?php echo $this->Commit->parents($log['parents'], $this->_request); ?>
-                    <?php echo $this->Commit->download($log['hash'], $this->_request); ?><br />
-                    <?php echo $this->Commit->diff($log['hash'], $this->_request); ?><br />
+                <td>
+                    <?php echo $blob['perm']; ?>
                 </td>
             </tr>
         <?php endforeach; ?>
